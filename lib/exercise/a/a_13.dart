f08bf570a06f78ef79c798181ae43774721d83cc
naokiwakata <rockwakata15@gmail.com>
2024-01-05 16:03:35 +0900

    fix a_13

--- lib/exercise/a/a_13.dart
+++ lib/exercise/a/a_13.dart
@@ -6,21 +6,21 @@ void main() {
   final K = NK[1];
 
   final A = inputIntList();
-  final R = List.generate(N, (index) => 0);
+  List<int> R = List.filled(N, 0);
 
   for (int i = 0; i < N - 1; i++) {
     if (i == 0)
-      R[i] == 1;
+      R[i] = 0;
     else
       R[i] = R[i - 1];
 
-    while (R[i] < N && A[R[i]] - A[i] <= K) {
+    while (R[i] < N - 1 && A[R[i] + 1] - A[i] <= K) {
       R[i]++;
     }
   }
 
   int answer = 0;
-  for (int i = 0; i < N; i++) {
+  for (int i = 0; i < N - 1; i++) {
     answer = answer + R[i] - i;
   }
   print(answer);
